207fcd35fb8d4e9545f09cd41a55e687b7219209
Tolik <t1005828@gmail.com>
2023-12-20 17:39:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/my/voicetext/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.my.voicetext
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            startSpeechToText()
+        }
+    }
+
+    private fun startSpeechToText() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ru-RU")
+        startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == Activity.RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val spokenText = result?.get(0) ?: ""
+            textView.text = spokenText
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_SPEECH_INPUT = 100
+    }
+
+}
